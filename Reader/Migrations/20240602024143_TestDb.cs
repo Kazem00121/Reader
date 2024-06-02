e2ef2e422bc7d27038f41db84628fefd547285c0
KaZem <Kazim00121@hotmail.com>
2024-06-02 06:15:09 +0330

    Creation of the first database

---
+++ Reader/Migrations/20240602024143_TestDb.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reader.Migrations
+{
+    public partial class TestDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    AuthorID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.AuthorID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Languages",
+                columns: table => new
+                {
+                    LanguageID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LanguageName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Languages", x => x.LanguageID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderStatuses",
+                columns: table => new
+                {
+                    OrderStatusID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderStatusName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderStatuses", x => x.OrderStatusID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Provices",
+                columns: table => new
+                {
+                    ProvinceID = table.Column<int>(type: "int", nullable: false),
+                    ProvinceName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Provices", x => x.ProvinceID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubCategories",
+                columns: table => new
+                {
+                    SubCategoryID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SubCategoryName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CategoryID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubCategories", x => x.SubCategoryID);
+                    table.ForeignKey(
+                        name: "FK_SubCategories_Categories_CategoryID",
+                        column: x => x.CategoryID,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    CityID = table.Column<int>(type: "int", nullable: false),
+                    CityName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProviceProvinceID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.CityID);
+                    table.ForeignKey(
+                        name: "FK_Cities_Provices_ProviceProvinceID",
+                        column: x => x.ProviceProvinceID,
+                        principalTable: "Provices",
+                        principalColumn: "ProvinceID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookInfo",
+                columns: table => new
+                {
+                    BookID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Summary = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false),
+                    File = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Image = table.Column<byte[]>(type: "image", nullable: true),
+                    LanguageID = table.Column<int>(type: "int", nullable: false),
+                    SCategoryID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookInfo", x => x.BookID);
+                    table.ForeignKey(
+                        name: "FK_BookInfo_Languages_LanguageID",
+                        column: x => x.LanguageID,
+                        principalTable: "Languages",
+                        principalColumn: "LanguageID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookInfo_SubCategories_SCategoryID",
+                        column: x => x.SCategoryID,
+                        principalTable: "SubCategories",
+                        principalColumn: "SubCategoryID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerID = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    FName = table.Column<string>(type: "nvarchar(20)", nullable: true),
+                    LName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Address1 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Mobile = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Tell = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City1CityID = table.Column<int>(type: "int", nullable: true),
+                    City2CityID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerID);
+                    table.ForeignKey(
+                        name: "FK_Customers_Cities_City1CityID",
+                        column: x => x.City1CityID,
+                        principalTable: "Cities",
+                        principalColumn: "CityID");
+                    table.ForeignKey(
+                        name: "FK_Customers_Cities_City2CityID",
+                        column: x => x.City2CityID,
+                        principalTable: "Cities",
+                        principalColumn: "CityID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Author_Books",
+                columns: table => new
+                {
+                    Author_BookID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookID = table.Column<int>(type: "int", nullable: false),
+                    AuthorID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Author_Books", x => x.Author_BookID);
+                    table.ForeignKey(
+                        name: "FK_Author_Books_Authors_AuthorID",
+                        column: x => x.AuthorID,
+                        principalTable: "Authors",
+                        principalColumn: "AuthorID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Author_Books_BookInfo_BookID",
+                        column: x => x.BookID,
+                        principalTable: "BookInfo",
+                        principalColumn: "BookID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Discounts",
+                columns: table => new
+                {
+                    BookID = table.Column<int>(type: "int", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Percent = table.Column<byte>(type: "tinyint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Discounts", x => x.BookID);
+                    table.ForeignKey(
+                        name: "FK_Discounts_BookInfo_BookID",
+                        column: x => x.BookID,
+                        principalTable: "BookInfo",
+                        principalColumn: "BookID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderID = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    AmountPaid = table.Column<long>(type: "bigint", nullable: false),
+                    DispatchNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BuyDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    OrderStatusID = table.Column<int>(type: "int", nullable: true),
+                    CustomerID = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderID);
+                    table.ForeignKey(
+                        name: "FK_Orders_Customers_CustomerID",
+                        column: x => x.CustomerID,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerID");
+                    table.ForeignKey(
+                        name: "FK_Orders_OrderStatuses_OrderStatusID",
+                        column: x => x.OrderStatusID,
+                        principalTable: "OrderStatuses",
+                        principalColumn: "OrderStatusID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order_Books",
+                columns: table => new
+                {
+                    Order_BookID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderID = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    BookID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order_Books", x => x.Order_BookID);
+                    table.ForeignKey(
+                        name: "FK_Order_Books_BookInfo_BookID",
+                        column: x => x.BookID,
+                        principalTable: "BookInfo",
+                        principalColumn: "BookID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Order_Books_Orders_OrderID",
+                        column: x => x.OrderID,
+                        principalTable: "Orders",
+                        principalColumn: "OrderID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Author_Books_AuthorID",
+                table: "Author_Books",
+                column: "AuthorID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Author_Books_BookID",
+                table: "Author_Books",
+                column: "BookID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookInfo_LanguageID",
+                table: "BookInfo",
+                column: "LanguageID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookInfo_SCategoryID",
+                table: "BookInfo",
+                column: "SCategoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_ProviceProvinceID",
+                table: "Cities",
+                column: "ProviceProvinceID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_City1CityID",
+                table: "Customers",
+                column: "City1CityID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_City2CityID",
+                table: "Customers",
+                column: "City2CityID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_Books_BookID",
+                table: "Order_Books",
+                column: "BookID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_Books_OrderID",
+                table: "Order_Books",
+                column: "OrderID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerID",
+                table: "Orders",
+                column: "CustomerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_OrderStatusID",
+                table: "Orders",
+                column: "OrderStatusID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubCategories_CategoryID",
+                table: "SubCategories",
+                column: "CategoryID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Author_Books");
+
+            migrationBuilder.DropTable(
+                name: "Discounts");
+
+            migrationBuilder.DropTable(
+                name: "Order_Books");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropTable(
+                name: "BookInfo");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Languages");
+
+            migrationBuilder.DropTable(
+                name: "SubCategories");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "OrderStatuses");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+
+            migrationBuilder.DropTable(
+                name: "Provices");
+        }
+    }
+}
---
+++ Reader/Models/ReaderContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Reader.Models
+{
+	public class ReaderContext: DbContext
+	{
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(local);Database=ReaderDB;Trusted_Connection=True");
+        }
+        DbSet<Book> Books { get; set; }
+        DbSet<Category> Categories { get; set; }
+        DbSet<SubCategory> SubCategories { get; set; }
+        DbSet<Order> Orders { get; set; }
+        DbSet<Author> Authors { get; set; }
+        DbSet<City> Cities { get; set; }
+        DbSet<Provice> Provices { get; set; }
+        DbSet<Author_Book> Author_Books { get; set; }
+        DbSet<Order_Book> Order_Books { get; set; }
+        DbSet<Language> Languages { get; set; }
+        DbSet<Discount> Discounts { get; set; }
+        DbSet<OrderStatus> OrderStatuses { get; set; }
+        DbSet<Customer> Customers { get; set; }
+
+    }
+}
